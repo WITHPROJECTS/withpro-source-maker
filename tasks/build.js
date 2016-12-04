305c00bfe9c9f081dd499ddf7b045137d7af40d3
Yokota Kenji <mail@yokotakenji.me>
2016-12-05 02:54:44 +0900

    v0.0.01

--- tasks/build.js
+++ tasks/build.js
@@ -1,10 +1,11 @@
-let gulp             = require('gulp');
-let path             = require('path');
-let minimist         = require('minimist');
-let gulpTaskRegister = require('./utils/gulp-task-register');
-let settingFile      = require(path.join(process.cwd(), 'setting.json'));
-let argv             = minimist(process.argv.slice(2));
-let type             = argv['t'] || argv['type'] ? argv['t'] || argv['type'] : 'default';
+let gulp                = require('gulp');
+let path                = require('path');
+let minimist            = require('minimist');
+let exec                = require('child-process-promise').exec;
+let withproGulpTaskRgtr = require('./utils/withpro-gulp-task-register');
+let settingFile         = require(path.join(process.cwd(), 'setting.json'));
+let argv                = minimist(process.argv.slice(2));
+let type                = argv['t'] || argv['type'] ? argv['t'] || argv['type'] : 'default';
 
 // =============================================================================
 // dependency module
@@ -19,11 +20,12 @@ let gulpBuild = ()=>{
   let modules = [gulpSass];
   let depends = [];
   modules.forEach((key)=> key.path = settingFile.path );
-  gulpTaskRegister(modules);
+  withproGulpTaskRgtr(modules);
   modules.forEach((obj)=> {
     for(let key in obj.functions) if(/build$/.test(key)) depends.push(key);
   });
   gulp.task('default', depends);
+  gulp.start('default');
 }
 // =============================================================================
 // default task
--- tasks/watch.js
+++ tasks/watch.js
@@ -1,3 +1,37 @@
-#!/usr/bin/env node
+let gulp             = require('gulp');
+let path             = require('path');
+let minimist         = require('minimist');
+let exec             = require('child-process-promise').exec;
+let gulpTaskRegister = require('./utils/gulp-task-register');
+let settingFile      = require(path.join(process.cwd(), 'setting.json'));
+let argv             = minimist(process.argv.slice(2));
+let type             = argv['t'] || argv['type'] ? argv['t'] || argv['type'] : 'default';
 
-console.log("watch!");
+// =============================================================================
+// dependency module
+// =============================================================================
+let gulpSass = require('withpro-gulp-sass');
+gulpSass.browsers = settingFile.modules['withpro-gulp-sass'].browsers;
+
+// =============================================================================
+// gulp task
+// =============================================================================
+let gulpBuild = ()=>{
+  let modules = [gulpSass];
+  let depends = [];
+  modules.forEach((key)=> key.path = settingFile.path );
+  gulpTaskRegister(modules);
+  modules.forEach((obj)=> {
+    for(let key in obj.functions) if(/watch/.test(key)) depends.push(key);
+  });
+  gulp.task('default', depends);
+  gulp.start('default');
+}
+// =============================================================================
+// default task
+// =============================================================================
+let build = ()=>{
+}
+
+if(type === 'gulp') gulpBuild();
+if(type === 'default') build();
